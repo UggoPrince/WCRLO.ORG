b7870fc9c9af016ce509ccb1badc28d5cb47e208
uggoprince <uggoprince@gmail.com>
2019-12-20 11:20:51 +0100

    update home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,35 +6,42 @@
    <section class="wcrlo-section">
       <div class="container-fluid">
          <div class="row">
-            <div class="col-md-4 wcrlo-animate2">
-               <div class="media">
+            <div class="col-md-4 wcrlo-animate2 text-center">
+               <div class="media text-left">
                      <div class="icon"><i class="flaticon-donation-1"></i></div>
-                     <div class="media-body">
+                     <div class="media-body p-2">
                         <h3 class="heading">Make Donation</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                        <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Make Donation</a></p>
+                        <p>
+                           Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+                        </p>
+                        
                      </div>
-               </div>     
+               </div>
+               <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Make Donation</a></p> 
             </div>
-            <div class="col-md-4 wcrlo-animate2">
-               <div class="media">
+            <div class="col-md-4 wcrlo-animate2 text-center">
+               <div class="media text-left">
                      <div class="icon"><i class="flaticon-charity"></i></div>
-                     <div class="media-body">
+                     <div class="media-body p-1">
                         <h3 class="heading">Become A Volunteer</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                        <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Become A Volunteer</a></p>
+                        <p>
+                           Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+                        </p>
                      </div>
-               </div>      
+               </div>
+               <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Become A Volunteer</a></p>     
             </div>
-            <div class="col-md-4 wcrlo-animate2">
-               <div class="media">
+            <div class="col-md-4 wcrlo-animate2 text-center">
+               <div class="media text-left">
                      <div class="icon"><i class="flaticon-donation"></i></div>
-                     <div class="media-body">
+                     <div class="media-body p-1">
                         <h3 class="heading">Sponsorship</h3>
-                        <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
-                        <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Sponsorship</a></p>
+                        <p>
+                           Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.
+                        </p>
                      </div>
-               </div>    
+               </div>
+               <p><a href="#" class="btn btn-white px-3 py-2 mt-2">Sponsorship</a></p>
          </div>
        </div>
       </div>
