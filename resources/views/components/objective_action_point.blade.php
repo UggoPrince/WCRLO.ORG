29ade71a890a477cae8595ac2d058eaede76aff5
uggoprince <uggoprince@gmail.com>
2019-12-21 17:43:20 +0100

    update home page with objectives and actions

--- resources/views/components/objective_action_point.blade.php
+++ resources/views/components/objective_action_point.blade.php
@@ -2,12 +2,12 @@
    data-setbg="images/bg_4.jpg">
    <div class="container-fluid">
       <div class="row">
-         <div class="col text-center mt-4 mb-5">
+         <div class="col text-center mb-5  wcrlo-animate2">
             <h3>OUR OBJECTIVES AND ACTION POINTS</h3>
          </div>
       </div>
       <div class="row">
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4  wcrlo-animate2">
             <div class="card p-2">
                <img class="card-img-top" src="images/wcrlo1-3.jpg" alt="Card image cap">
                <div class="card-body">
@@ -15,7 +15,7 @@
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
                <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
                <div class="card-body">
@@ -23,23 +23,23 @@
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-4.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Child Education</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-5.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Domestic Violence against Women & Chilldren</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
                <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
                <div class="card-body">
@@ -47,33 +47,33 @@
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-6.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Employment discrimination against Women</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-4.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Fight against Sexual Violence and Rape</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-5.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Women/Children Health care</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-6.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">
                       Eradication of Poverty for Women through small loan & 
@@ -81,17 +81,17 @@
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-7.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">Senitisitation & public enlightement programs for women& children</h5>
                </div>
             </div>
          </div>
-         <div class="col-md-3 wcrlo-card mb-4">
+         <div class="col-md-3 wcrlo-card mb-4 wcrlo-animate2">
             <div class="card p-2">
-               <img class="card-img-top" src="images/wcrlo1-2.jpg" alt="Card image cap">
+               <img class="card-img-top" src="images/wcrlo1-8.jpg" alt="Card image cap">
                <div class="card-body">
                   <h5 class="card-title">
                       Provision of food and other necessities to charity homes like orphanage, 
